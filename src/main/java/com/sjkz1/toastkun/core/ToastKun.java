92266d94378a7dbb2dec8959ae02dff28d808248
SJKZ1-2565 <seefuture13@gmail.com>
2021-01-24 15:18:10 +0700

    Remove SteveKung's Lib

--- src/main/java/com/sjkz1/toastkun/core/ToastKun.java
+++ src/main/java/com/sjkz1/toastkun/core/ToastKun.java
@@ -3,12 +3,9 @@ package com.sjkz1.toastkun.core;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.sjkz1.toastkun.event.ClientEventsBus;
 import com.sjkz1.toastkun.event.OreGen;
 import com.sjkz1.toastkun.init.TKBlocks;
 import com.sjkz1.toastkun.init.TKItems;
-import com.stevekung.stevekungslib.utils.CommonUtils;
-
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -28,7 +25,6 @@ public class ToastKun
 		Bus.addListener(this::setup);
 		TKItems.ITEMS.register(Bus);
 		TKBlocks.BLOCKS.register(Bus);
-		CommonUtils.registerEventHandler(new ClientEventsBus());
 		MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, OreGen::genOres);
 		LOGGER.info("Successfully setup mod!");
 	}
--- src/main/java/com/sjkz1/toastkun/event/ClientEventsBus.java
+++
@@ -1,7 +0,0 @@
-package com.sjkz1.toastkun.event;
-
-public class ClientEventsBus 
-{
-	
-	
-}
